2338d449a94044abbf7aa5e94c22c8d5764f3799
the-nostromo-1 <tinman11537@gmail.com>
2023-02-05 17:14:57 -0500

    Milestone 1 refactoring

--- bigint.cpp
+++ bigint.cpp
@@ -6,34 +6,27 @@ David J Tinley
 
 #include "bigint.hpp"
 
-bigint::bigint() // default constructor
-{
-    for (int i = 0; i < CAPACITY; ++i) // filling bigint array with 0's
-    {
+bigint::bigint() { // default constructor
+    for (int i = 0; i < CAPACITY; ++i) { // filling bigint array with 0's
         big_int_array[i] = 0;
     }
 }
 
-bigint::bigint(int x) : bigint() // constructor initializing int with user choice
-{
-    for (int i = 0; i < CAPACITY; ++i)
-    {
+bigint::bigint(int x) : bigint() { // constructor initializing int with user choice
+    for (int i = 0; i < CAPACITY; ++i) {
         big_int_array[i] = x % 10;
         x /= 10;
     }
 }
 
-bigint::bigint(const char bg_array[]) : bigint() // : bigint() calls default constructor before newly defined constructor
-{
+bigint::bigint(const char bg_array[]) : bigint() { // : bigint() calls default constructor before newly defined constructor
     int length = 0;
-    while (bg_array[length] != '\0') // '\0' = null terminating character
-    {
+    while (bg_array[length] != '\0') { // '\0' = null terminating character
         ++length;
     }
     int looping_tracker = length;
     int number;
-    for (int j = 0; j < looping_tracker; ++j)
-    {
+    for (int j = 0; j < looping_tracker; ++j) {
         number = int(bg_array[length - 1] - int('0'));
         big_int_array[j] = number;
         --length;
@@ -44,47 +37,36 @@ bigint::bigint(const char bg_array[]) : bigint() // : bigint() calls default con
     // 0 = 48, 1 = 49, 2 = 50, 3 = 51, 4 = 52, 5 = 53, 6 = 54, 7 = 55, 8 = 56, 9 = 57
 }
 
-bool bigint::operator==(const bigint &rhs)
-{
-    for (int i = 0; i < CAPACITY; ++i)
-    {
-        if (big_int_array[i] != rhs.big_int_array[i])
-        {
+bool bigint::operator==(const bigint &rhs) {
+    for (int i = 0; i < CAPACITY; ++i) {
+        if (big_int_array[i] != rhs.big_int_array[i]) {
             return false;
         }
     }
     return true;
 }
 
-std::ostream &operator<<(std::ostream &out, const bigint &rhs) // insertion operator overload
-{
+std::ostream &operator<<(std::ostream &out, const bigint &rhs) { // insertion operator overload
     int j = CAPACITY;
-    while (rhs.big_int_array[j - 1] == 0)
-    {
+    while (rhs.big_int_array[j - 1] == 0) {
         --j;
     }
     int n_capacity = j - 80;
-    if (j <= 0)
-    {
+    if (j <= 0) {
         out << "0" << std::endl;
     }
-    if (j > 80)
-    {
-        for (int i = j - 1; i > n_capacity; --i)
-        {
+    if (j > 80) {
+        for (int i = j - 1; i > n_capacity; --i) {
             out << rhs.big_int_array[i];
         }
         out << std::endl;
-        for (int i = n_capacity; i >= 0; --i)
-        {
+        for (int i = n_capacity; i >= 0; --i) {
             out << rhs.big_int_array[i];
         }
         out << std::endl;
     }
-    else
-    {
-        for (int i = j - 1; i >= 0; --i)
-        {
+    else {
+        for (int i = j - 1; i >= 0; --i) {
             out << rhs.big_int_array[i];
         }
         out << std::endl;
@@ -92,10 +74,8 @@ std::ostream &operator<<(std::ostream &out, const bigint &rhs) // insertion oper
     return out;
 }
 
-void bigint::debugPrint(std::ostream &out) const
-{
-    for (int i = CAPACITY - 1; i > 0; --i)
-    {
+void bigint::debugPrint(std::ostream &out) const {
+    for (int i = CAPACITY - 1; i > 0; --i) {
         out << big_int_array[i] << " | ";
     }
 }
--- bigint.hpp
+++ bigint.hpp
@@ -11,17 +11,16 @@ David J Tinley
 
 const int CAPACITY = 200;
 
-class bigint
-{
-public:
-    bigint();    // default constructor
-    bigint(int); // constructor initializing int values in bigint array from user input
-    bigint(const char bg_array[]);
-    bool operator==(const bigint &rhs); // equality operator overload
-    void debugPrint(std::ostream &) const;
-    friend std::ostream &operator<<(std::ostream &out, const bigint &rhs); // insertion operator overload
-private:
-    int big_int_array[CAPACITY];
+class bigint {
+    public:
+        bigint();    // default constructor
+        bigint(int); // constructor initializing int values in bigint array from user input
+        bigint(const char bg_array[]);
+        bool operator==(const bigint &rhs); // equality operator overload
+        void debugPrint(std::ostream &) const;
+        friend std::ostream &operator<<(std::ostream &out, const bigint &rhs); // insertion operator overload
+    private:
+        int big_int_array[CAPACITY];
 };
 
 #endif
